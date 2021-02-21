c7cb00a4fed2ff4b774596457b7a707b3a2383c8
dayuhidayatullah <dayuhidayat123@gmail.com>
2021-02-21 08:09:57 +0700

    dayu/tdd-user-login/spotify-login

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
+
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
 // require
 const express = require('express')
+const cors = require('cors')
 const app = express()
-const port = 6300
 
 const UserRouter = require('./routes/user-route')
 const FavGenreRouter = require('./routes/fav-route')
+const errorHandler = require('./middlewares/errorHandler')
 
 // setting
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 // routing
 app.use(UserRouter)
 app.use(FavGenreRouter)
+app.use(errorHandler)
 
 // listening
-app.listen(port, () => {
-    console.log('listening on port ', port)
-})
+
+module.exports = app
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,6 +2,9 @@ const { User } = require('../models')
 const { Op } = require("sequelize")
 const { comparePassword } = require('../helpers/bcrypt')
 const { generateToken } = require('../helpers/jwt')
+const querystring = require('querystring')
+const redirect_uri = 'http://localhost:6300/callbacks'
+
 
 class Controller {
     static register(req, res, next) {
@@ -10,7 +13,6 @@ class Controller {
             username: req.body.username,
             password: req.body.password
         }
-
         User.create(user)
             .then(data => {
                 const sent = {
@@ -27,13 +29,13 @@ class Controller {
                 //     code: 500,
                 //     from: 'Controller user: register user'
                 // })
-                next()
+                next(err)
             })
     }
 
     static login(req, res, next) {
         const { validator, password } = req.body
-
+        console.log(validator)
         User.findOne({
             where: {
                 [Op.or]: [
@@ -44,11 +46,10 @@ class Controller {
         })
             .then(data => {
                 if (!data) {
-                    // next({
-                    //     message: 'Invalid email/password',
-                    //     code: 401,
-                    //     from: 'Controller User: login user'
-                    // })
+                    next({
+                        name: 'invalidLogin',
+                        code: 401
+                    })
                     next()
                 }
 
@@ -63,11 +64,10 @@ class Controller {
                     const access_token = generateToken(payload)
                     res.status(200).json({ access_token })
                 } else {
-                    // next({
-                    //     message: 'Invalid email/password',
-                    //     code: 401,
-                    //     from: 'Controller User: login user'
-                    // })
+                    next({
+                        name: 'invalidLogin',
+                        code: 401,
+                    })
                     next()
                 }
 
@@ -78,8 +78,87 @@ class Controller {
                 //     code: 400,
                 //     from: 'Controller User: login user'
                 // })
-                next()
+                next(err)
+                // res.status(500).json('salah')
             })
+    }
+    static loginSpotify(req, res, next) {
+        // console.log(process.env.SPOTIFY_CLIENT_ID)
+        res.redirect('https://accounts.spotify.com/authorize?' +
+            querystring.stringify({
+            response_type: 'code',
+            client_id: process.env.SPOTIFY_CLIENT_ID,
+            scope: 'user-read-private user-read-email',
+            redirect_uri
+        }))
+    }
+    static callbackLoginSpotify(req, res, next){
+        let code = req.query.code || null
+        let authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+            code: code,
+            redirect_uri,
+            grant_type: 'authorization_code'
+            },
+            headers: {
+            'Authorization': 'Basic ' + (new Buffer(
+                process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+            ).toString('base64'))
+            },
+            json: true
+        }
+        const request = require('request')
+        request.post(authOptions, function(error, response, body) {
+            var access_token = body.access_token
+            var refresh_token = body.refresh_token
+            console.log(refresh_token, '<<<< ini refresh tokenya ')
+            let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
+            const option = {
+                url: 'https://api.spotify.com/v1/me',
+                headers: { 'Authorization': 'Bearer ' + access_token },
+                json: true
+            }
+            // https://accounts.spotify.com/id/login?continue=https:%2F%2Faccounts.spotify.com%2Fauthorize%3Fscope%3Duser-read-private%2Buser-read-email%26response_type%3Dcode%26redirect_uri%3Dhttp%253A%252F%252Flocalhost%253A6300%252Fcallbacks%26client_id%3Dd0574f1b62714617865db1237adeb779
+            const axios = require('axios')
+            let user;
+            axios.get('https://api.spotify.com/v1/me', {
+                headers: {
+                    'Authorization': 'Bearer ' + access_token
+                }
+            })
+                .then(({data}) => {
+                    user = data
+                    return User.findOne({
+                        where: {
+                            email: data.email
+                        }
+                    })
+                })
+                .then(userData => {
+                    if(userData){
+                        return userData
+                    }
+                    else {
+                        const username = user.email.split('@')
+                        console.log(userData, '<<<< ini user data', user, '<<<<<< ini data global', 'then2')
+                        return User.create({ email: user.email, username: username[0], password: 'random' })
+                    }
+                })
+                .then(userData => {
+                    console.log(userData, '<<<< ini user data', user, '<<<<<< ini data global', 'then3')
+                    const access_token_spotify = access_token
+                    
+                    const access_token_local = generateToken({email: userData.email, id: userData.id})
+                    res.redirect(uri + '?access_token=' + access_token + '&access_token_lokal=' + access_token_local)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+                // request.get(option, function(error, response, body) {
+                    //     console.log(body)
+                    // })
+        })
     }
 }
 
