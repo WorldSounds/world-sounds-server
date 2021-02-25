8baff42a99af589ffb5e7e932dc727f42628a315
samm021 <mohammed.ismail.pribadi@gmail.com>
2021-02-26 03:52:32 +0700

    second try deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'worldsounds',
       script: 'npm install && nodemon server.js',
       env: {
-        REDIRECT_URI: "ec2-54-179-61-252.ap-southeast-1.compute.amazonaws.com:6300/callbacks",
+        REDIRECT_URI: "http://54.179.61.252:6300/callbacks",
         SPOTIFY_CLIENT_ID: 'd0574f1b62714617865db1237adeb779',
         SPOTIFY_CLIENT_SECRET: '9e789f302de64069be1f7d71d07ffa3b',
         PORT: 6300,
